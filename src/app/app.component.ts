d9a8142df03c8522cbd7d1ddd658ce8057a99bb2
Alan <alannavaseg_02@hotmail.com>
2020-05-04 15:07:16 -0500

    Se termina la aplicacion de votos con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Link } from './link/link.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-links-vote-app';
+  links: Link[];
+  constructor(){
+    this.links = [
+      new Link('Angular', 'https://angular.io', 12),
+      new Link('Google', 'https://google.com', 122),
+      new Link('Facebook', 'https://facebook.com', 18)
+    ];
+  }
+  addLink(title: HTMLInputElement, link: HTMLInputElement){
+    this.links.push(new Link(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
 }
